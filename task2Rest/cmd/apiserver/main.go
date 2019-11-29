326aaa5fdd923e745d3d437e3c24e386e24b616e
Chensimeh <cepnmok@gmail.com>
2019-11-29 17:32:12 +0300

    apiserver cofig done

--- task2Rest/cmd/apiserver/main.go
+++ task2Rest/cmd/apiserver/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"flag"
+	"github.com/trzhensimekh/cursesGo/task2Rest/internal/app/apiserver"
+	"log"
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	configPath string
+)
+
+func init(){
+	flag.StringVar(&configPath,"config-path","configs/apiserver.toml","config file path")
+}
+
 func main() {
-	println("hello, world")
+	flag.Parse()
+	config :=apiserver.NewConfig()
+	_ , err := toml.DecodeFile(configPath,config)
+	if err !=nil {
+		log.Fatal(err)
+	}
+
+
+	s:=apiserver.New(config)
+	if err := s.Start(); err!=nil {
+		log.Fatal(err)
+	}
+	println("hello, apiserver")
 }
--- task2Rest/internal/app/apiserver/apiserver.go
+++ task2Rest/internal/app/apiserver/apiserver.go
@@ -1,1 +1,18 @@
 package apiserver
+
+//APIServer ...
+type APIServer struct{
+	config *Config
+}
+
+// New ...
+func New(config*Config)*APIServer {
+	return &APIServer{
+		config: config,
+	}
+}
+
+// Start ...
+func (*APIServer)Start() error {
+	return nil
+}
